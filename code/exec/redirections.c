15ef3121a26e4067b9eef9bed2e3a1249049d60d
fakambou <fakambou@student.42nice.fr>
2025-05-14 15:27:18 +0200

    push iheb 42

--- code/exec/redirections.c
+++ code/exec/redirections.c
@@ -6,10 +6,16 @@
 /*   By: fakambou <fakambou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 18:05:15 by fakambou          #+#    #+#             */
-/*   Updated: 2025/05/13 18:07:20 by fakambou         ###   ########.fr       */
+/*   Updated: 2025/05/13 19:19:29 by fakambou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../resources/minishell.h"
 
-vo
+void	redirect_in(t_lexer *redirect)
+{
+	if (redirect->token_type == REDIREC_IN)
+	{
+		redirect->next->str 
+	}
+}
